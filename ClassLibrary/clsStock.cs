b3027b65f41a54534e389980389b7925eb324f19
Tamir Aydros <P2595613@my365.dmu.ac.uk>
2022-08-12 07:19:18 +0100

    created properties for table design

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public int StockID { get; set; }
+        public string StockName { get; set; }
+        public int StockQuantity { get; set; }
+        public int StockTotalSold { get; set; }
+        public DateTime DateAdded { get; set; }
+        public decimal Price { get; set; }
+        public bool StockAvailable { get; set; }
     }
 }
