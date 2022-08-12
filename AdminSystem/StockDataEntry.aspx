9867a00e926ee18f1916a2fe5c31c377fc94f63a
Tamir Aydros <P2595613@my365.dmu.ac.uk>
2022-08-12 11:55:19 +0100

    final

--- AdminSystem/StockDataEntry.aspx
+++ AdminSystem/StockDataEntry.aspx
@@ -7,10 +7,36 @@
     <title></title>
 </head>
 <body>
-    This is Stock entry page
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:Label ID="lblStockID" runat="server" Text="StockID"></asp:Label>
+        <asp:TextBox ID="txtStockID" runat="server" style="margin-left: 50px" Width="216px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblStockName" runat="server" Text="StockName" width="64px"></asp:Label>
+        <asp:TextBox ID="txtStockName" runat="server" style="margin-left: 50px" width="218px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblStockQuantity" runat="server" Text="StockQuantity" width="64px"></asp:Label>
+        <asp:TextBox ID="txtStockQuantity" runat="server" style="margin-left: 50px" width="218px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblTotalSold" runat="server" Text="TotalSold" width="64px"></asp:Label>
+        <asp:TextBox ID="txtTotalSold" runat="server" style="margin-left: 50px" width="218px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblDateAdded" runat="server" Text="DateAdded" width="64px"></asp:Label>
+        <asp:TextBox ID="txtDateAdded" runat="server" style="margin-left: 50px" width="218px"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblPrice" runat="server" Text="Price" width="64px"></asp:Label>
+        <asp:TextBox ID="txtPrice" runat="server" style="margin-left: 50px" width="218px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:CheckBox ID="chkAvailable" runat="server" OnCheckedChanged="chkAvailable_CheckedChanged" Text="Available" />
+        <br />
+        <br />
+        <asp:Label ID="lblError" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnOK" runat="server" OnClick="btnOK_Click" Text="OK" />
+&nbsp;<asp:Button ID="btnCancel" runat="server" Text="Cancel" />
     </form>
 </body>
 </html>
--- Testing1/tstStock.cs
+++ Testing1/tstStock.cs
@@ -80,5 +80,127 @@ namespace Testing1
             AStock.StockAvailable = TestData;
             Assert.AreEqual(AStock.StockAvailable, TestData);
         }
+        //Find Method//
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+
+        public void TestStockIDFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.StockID != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void TestStockNameFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.StockName != "Nike")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void TestStockQuantityFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.StockQuantity != 1)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void DateAddedFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.DateAdded != Convert.ToDateTime("01/04/2022"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void TestPriceFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.Price != 20)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+
+        public void TestStockAvailableFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.StockAvailable != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+
+        public void TestTotalStockSoldFound()
+        {
+            clsStock AStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 1;
+            Found = AStock.Find(StockID);
+            if (AStock.StockTotalSold != 20)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
     }
-}
+    }
