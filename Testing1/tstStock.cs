29aec1fe0b9db7d112b867705fe750ecd6275430
Tamir Aydros <P2595613@my365.dmu.ac.uk>
2022-08-12 06:45:54 +0100

    Added testing for address class

---
+++ Testing1/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStock AStock = new clsStock();
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
